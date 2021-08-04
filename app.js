b14651e6de37b18d44d64bb5d922ab9bc312d745
Jake Gardner <gardner.761@gmail.com>
2021-08-04 12:55:56 -0600

    Push to github

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ tl.fromTo(".emphasize-text", { opacity: 0 }, { color: "darkviolet", opacity: 1, 
 tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 tl.to(".intro", {opacity: 0, display:"none"});
-tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".nav-container", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: .75 });
 tl.to(".big-text", { opacity: 0, duration: 2},);
