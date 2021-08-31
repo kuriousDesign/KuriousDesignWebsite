e8a0f0e6380392899ee86b20758c958224cf6f2d
Jake Gardner <gardner.761@gmail.com>
2021-08-30 22:18:38 -0600

    fixed contact links

---
+++ contactform.php
@@ -0,0 +1,16 @@
+<?php
+
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+$mailTo = "jake@kurious-design.com";
+$headers = "From kurious-design.com: ".$subject;
+$txt = "You have received an email from ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
